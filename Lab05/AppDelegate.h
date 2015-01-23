93ad1367085c1a4accf02409650c7b10bf7fc5c2
jvlioverne <jvlioverne@gmail.com>
2015-01-23 14:52:08 -0600

    Initial Commit

---
+++ Lab05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab05
+//
+//  Created by Benjas on 23/01/15.
+//  Copyright (c) 2015 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
